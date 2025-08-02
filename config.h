5245bcf8e46e4cbe91d89eefd3b926b4345e6ac3
NextiaIndex <nextiaindex@protonmail.com>
2025-08-02 16:58:08 +0700

    change font to Nathanielevan's Iosevka and add fallback nerd font mono

--- config.h
+++ config.h
@@ -5,12 +5,11 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Iosevka Custom:pixelsize=15:antialias=true:autohint=true";
 #if FONT2_PATCH
 /* Spare fonts */
 static char *font2[] = {
-/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
-/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+	"JetBrainsMono Nerd Font Mono:pixelsize=15:antialias=true:autohint=true",
 };
 #endif // FONT2_PATCH
 
